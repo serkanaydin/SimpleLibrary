5c62514cf1a9e7f90c916fb323fca785ed45461f
serkanaydin <serkanaydin@marun.edu.tr>
2021-10-18 14:30:52 +0300

    Proje dosyası ekle.

---
+++ Domain/SimpleLibrary.Domain/Book/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLibrary.Domain.Book
+{
+    public class Book 
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
